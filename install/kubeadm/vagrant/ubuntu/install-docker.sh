d496d167cf7b746c68f865c95a02b4c68f6b1dce
Silvio Silva <smsilva@gmail.com>
2020-04-14 03:55:42 -0300

    Worker Node

--- install/kubeadm/vagrant/ubuntu/install-docker.sh
+++ install/kubeadm/vagrant/ubuntu/install-docker.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive 
-apt-get update \
-    && apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        software-properties-common \
-    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - \
-    && add-apt-repository \
-        "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-        $(lsb_release -cs) \
-        stable" \
-    && apt-get update \
-    && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
+cd /tmp
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh /tmp/get-docker.sh
