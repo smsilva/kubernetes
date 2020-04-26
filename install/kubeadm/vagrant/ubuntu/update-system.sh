b9cf32b0d19c961df0cd4c1f37ddfbecebd08fc1
Silvio Silva <smsilva@gmail.com>
2020-04-26 12:58:17 -0300

    Update não interativo

--- install/kubeadm/vagrant/ubuntu/update-system.sh
+++ install/kubeadm/vagrant/ubuntu/update-system.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-apt-get update -y && \
-apt-get upgrade -y && \
-apt-get dist-upgrade -y && \
-apt-get autoremove
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+# apt-get update -y && \
+# apt-get upgrade -y && \
+# apt-get dist-upgrade -y && \
+# apt-get autoremove
