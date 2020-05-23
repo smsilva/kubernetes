071e9207e7f9382c47c2fec4b0882c97743767d3
Silvio Silva <smsilva@gmail.com>
2020-05-23 16:08:10 -0300

    TMUX Scripts

--- install/kubeadm/vagrant/tmux-get-server-name.sh
+++ install/kubeadm/vagrant/tmux-get-server-name.sh
@@ -11,3 +11,11 @@ servers['11']="worker-2"
 INDEX=$(echo -n ${TMUX_PANE} | tr -d "%")
 
 echo -n "${servers[${INDEX}]}"
+
+tmux list-panes | while read line; do
+  VALUES=$(echo $line | awk '{ print $1 " " $7 }' | sed 's/: %/-/')
+  LINE=$(echo ${VALUES} | awk -F "-" '{ print $1 }')
+  echo "${VALUES} - PANE: ${TMUX_PANE}"
+
+
+done
--- install/kubeadm/vagrant/tmux-vagrant-connect-ssh.sh
+++ install/kubeadm/vagrant/tmux-vagrant-connect-ssh.sh
@@ -3,4 +3,5 @@
 SERVER_NAME="$(./tmux-get-server-name.sh)"
 
 echo "vagrant ssh ${SERVER_NAME}..."
+
 sshvg ${SERVER_NAME}
