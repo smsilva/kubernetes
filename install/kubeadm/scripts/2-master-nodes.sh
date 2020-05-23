a3edca03003b0e3bad2dbe2f54ef3eba23f1afc1
Silvio Silva <smsilva@gmail.com>
2020-05-23 11:10:07 -0300

    Atualizando scripts

--- install/kubeadm/scripts/2-master-nodes.sh
+++ install/kubeadm/scripts/2-master-nodes.sh
@@ -32,38 +32,6 @@ printf '%d hour %d minute %d seconds\n' $((${SECONDS}/3600)) $((${SECONDS}%3600/
 # Set Default Namespace to kube-system
 kubectl config set-context --current --namespace kube-system
 
-# Configure PATH
-echo "PATH=${PATH}:${HOME}/bin/" >> ~/.bashrc && source ~/.bashrc && mkdir "${HOME}/bin"
-
-# Custom Columns Template
-CUSTOM_COLUMNS_TEMPLATE_DIRECTORY="${HOME}/.kube/templates"
-CUSTOM_COLUMNS_NODES_FILE="custom-columns-nodes.template"
-
-mkdir -p "${CUSTOM_COLUMNS_TEMPLATE_DIRECTORY}"
-
-cat <<EOF > "${CUSTOM_COLUMNS_TEMPLATE_DIRECTORY}/${CUSTOM_COLUMNS_NODES_FILE}"
-NAME           STATUS                                               INTERNAL_IP                                        VERSION
-.metadata.name .status.conditions[?(@.type=="Ready")].reason .status.addresses[?(@.type=="InternalIP")].address .status.nodeInfo.kubeletVersion
-EOF
-
-cat <<EOF > ~/bin/custom-columns-config-templates
-#!/bin/bash
-TEMPLATE_DIRECTORY="${CUSTOM_COLUMNS_TEMPLATE_DIRECTORY}"
-CUSTOM_COLUMNS_NODES_FILE="\${TEMPLATE_DIRECTORY}/${CUSTOM_COLUMNS_NODES_FILE}"
-EOF
-
-cat <<EOF > ~/bin/kubectl-nodes
-#!/bin/bash
-. custom-columns-config-templates
-
-NODES=\$*
-
-kubectl get nodes \${NODES} \\
-  --output custom-columns-file="\${CUSTOM_COLUMNS_NODES_FILE}" | sed 's/KubeletReady/Ready/;s/NodeStatusUnknown/NotReady/;' | column -t
-EOF
-
-chmod +x ~/bin/*
-
 # Watch Nodes and Pods from kube-system namespace
 watch -n 3 '
   kubectl nodes && \
