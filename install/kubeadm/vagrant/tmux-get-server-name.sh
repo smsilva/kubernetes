713cbf0154713e55a7de768e021e92da3697d69d
Silvio Silva <smsilva@gmail.com>
2020-05-26 20:49:51 -0300

    Se houver uma janela all o comando conn.sh conectará corretamente nos masters e workers

--- install/kubeadm/vagrant/tmux-get-server-name.sh
+++ install/kubeadm/vagrant/tmux-get-server-name.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+WINDOW_NAME=$(tmux list-windows | grep active | awk '{ print $2 }' | sed 's/^all.*/all/')
 NODE_NAME=$(tmux list-windows | grep active | awk '{ print $2 }' | sed 's/^masters.*/master/;s/^workers.*/worker/')
+
+export NODES=$(vagrant status | grep -E "^master|^worker" | awk '{ print $1 }')
 
 tmux list-panes | while read line; do
   VALUES=$(echo $line | awk '{ print $1 " " $7 }' | sed 's/: %/-/')
@@ -9,6 +12,26 @@ tmux list-panes | while read line; do
   ACTUAL_PANE_NUMBER=$(echo -n ${TMUX_PANE} | sed 's/%//')
   
   if [[ ((${PANE_NUMBER} == ${ACTUAL_PANE_NUMBER})) ]]; then
-    echo "${NODE_NAME}-$((${LINE_NUMBER}+1))"
+    if [[ "${WINDOW_NAME}" == "all" ]]; then
+      SERVERS_COUNT=0
+      MASTERS_COUNT=0
+      WORKERS_COUNT=0
+      for node in ${NODES}; do
+        ((SERVERS_COUNT++))
+        if [[ ${node} =~ ^master ]]; then
+          ((MASTERS_COUNT++))
+          NODE_NAME="master-${MASTERS_COUNT}"
+        else
+          ((WORKERS_COUNT++))
+          NODE_NAME="worker-${WORKERS_COUNT}"
+        fi
+        
+        if [[ ((${SERVERS_COUNT} == $((${LINE_NUMBER}+1)))) ]]; then
+          echo "${NODE_NAME}"
+        fi
+      done
+    else
+      echo "${NODE_NAME}-$((${LINE_NUMBER}+1))"
+    fi
   fi
 done
