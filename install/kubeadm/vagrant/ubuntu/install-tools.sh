797fa07c6fd3fdba80eb81fbaf335f2943dfa0c6
Silvio Silva <smsilva@gmail.com>
2020-06-10 08:10:41 -0300

    Simplificando os scripts para deixar apenas o que está relacionado à instalação do Kubernetes

---
+++ install/kubeadm/vagrant/ubuntu/install-tools.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Configure Vim to use yaml format a little bit better
+cat <<EOF >> .vimrc
+autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
+EOF
+
+# bat
+BAT_VERSION="0.15.1" && \
+BAT_DEB_FILE="bat_${BAT_VERSION}_amd64.deb" && \
+wget "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/${BAT_DEB_FILE}" \
+  --output-document "${BAT_DEB_FILE}" && \
+sudo dpkg -i "${BAT_DEB_FILE}" && rm "${BAT_DEB_FILE}" && \
+echo "alias cat='bat -p'" >> ~/.bash_aliases && source ~/.bash_aliases && bat --version
+
+# jq
+sudo apt-get install jq --yes && jq --version
+
+# yq
+sudo snap install yq
+
+yq --version
+
+echo "alias yq='yq -C -P'" >> ~/.bashrc && source ~/.bashrc
+
+# ytop
+VERSION="0.6.2"
+
+TAR_FILE="ytop-${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+wget "https://github.com/cjbassi/ytop/releases/download/0.6.2/${TAR_FILE}"
+
+tar xvf "${TAR_FILE}" && \
+mv ytop /usr/bin/ && \
+rm "${TAR_FILE}"
