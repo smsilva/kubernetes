6a5c93111599ef476dd50493cb3da95d2617b864
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-06-16 20:32:24 -0300

    Atualizando conteúdo da pasta shared

--- install/kubeadm/multipass/shared/update-system-config-all.sh
+++
@@ -1,3 +0,0 @@
-multipass exec loadbalancer -- sudo /shared/update-system-config.sh
-multipass exec master-1 -- sudo /shared/update-system-config.sh
-multipass exec worker-1 -- sudo /shared/update-system-config.sh
