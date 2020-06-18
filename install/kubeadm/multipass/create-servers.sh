9ae27f98adf590f5b0c07b030b3574833aa7af9a
Silvio Silva <smsilva@gmail.com>
2020-06-18 08:01:27 -0300

    Refatorando scripts

---
+++ install/kubeadm/multipass/create-servers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+./check-environment-variables.sh
+
+for SERVER in ${SERVERS}; do
+  SERVER_PREFIX_KEY_NAME=$(awk -F '-' '{ print $1 }' <<< "${SERVER}")
+
+  SERVER_MEMORY_KEY="${SERVER_PREFIX_KEY_NAME^^}_MEMORY"
+  SERVER_CPU_COUNT_KEY="${SERVER_PREFIX_KEY_NAME^^}_CPU_COUNT"
+  SERVER_DISK_SIZE_KEY="${SERVER_PREFIX_KEY_NAME^^}_DISK_SIZE"
+
+  echo "${SERVER}.${DOMAIN_NAME} (vcpu: ${!SERVER_CPU_COUNT_KEY} / mem: ${!SERVER_MEMORY_KEY} / disk: ${!SERVER_DISK_SIZE_KEY})"
+
+  multipass launch \
+    --cpus "${!SERVER_CPU_COUNT_KEY}" \
+    --disk "${!SERVER_DISK_SIZE_KEY}" \
+    --mem "${!SERVER_MEMORY_KEY}" \
+    --name "${SERVER}" \
+    --cloud-init "${CLOUD_INIT_TARGET_DIRECTORY}/${SERVER}.yaml" && \
+  multipass mount "shared/" "${SERVER}":"/shared"
+done
--- install/kubeadm/multipass/generate-cloud-init-files.sh
+++ install/kubeadm/multipass/generate-cloud-init-files.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 ./check-environment-variables.sh
 
-TARGET_DIRECTORY="cloud-init"
-TEMPLATE_DIRECTORY="templates/cloud-init"
+mkdir --parents "${CLOUD_INIT_TARGET_DIRECTORY}"
 
-mkdir --parents "${TARGET_DIRECTORY}"
-
-rm -rf "${TARGET_DIRECTORY}/*.yaml"
+rm -rf "${CLOUD_INIT_TARGET_DIRECTORY}/*.yaml"
 
 for SERVER in ${SERVERS}; do
   export SERVER_HOST_NAME="${SERVER}"
   
   CLOUD_INIT_TEMPLATE_NAME=$(awk -F '-' '{ print $1 }' <<< "${SERVER}")
   
-  CLOUD_INIT_FILE="${TARGET_DIRECTORY}/${SERVER}.yaml"
+  CLOUD_INIT_FILE="${CLOUD_INIT_TARGET_DIRECTORY}/${SERVER}.yaml"
   
-  cat "${TEMPLATE_DIRECTORY}/${CLOUD_INIT_TEMPLATE_NAME}.yaml" | envsubst > "${CLOUD_INIT_FILE}"
+  cat "${CLOUD_INIT_TEMPLATE_DIRECTORY}/${CLOUD_INIT_TEMPLATE_NAME}.yaml" | envsubst > "${CLOUD_INIT_FILE}"
 
   echo "${CLOUD_INIT_FILE}"
 done
