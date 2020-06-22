3ad0a2433f40704af9b5f39addd1709b64536277
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-06-22 13:11:53 -0300

    Comando para configurar as variáveis de ambiente

---
+++ install/kubeadm/multipass/set-multipass-cidr-in-environment.conf-file.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+primary_create() {
+  multipass start primary
+}
+
+primary_start() {
+  multipass start primary
+}
+
+primary_stop() {
+  # multipass stop primary
+  echo "####Fake Stop####"
+}
+
+primary_status() {
+  STATUS=$(multipass info primary | grep "State" | awk '{ print $2 }')
+
+  echo "${STATUS}"
+}
+
+config_environment_conf_file() {
+  MULTIPASS_PRIMARY_MACHINE_IP=$(multipass list | grep -E "^primary" | awk '{ print $3 }')
+
+  sed --in-place "/.*IP_NETWORK=.*/ s/=.*$/=\"${MULTIPASS_PRIMARY_MACHINE_IP}\"/" environment.conf
+
+  echo "done"
+}
+
+if ! multipass list | grep -E "^primary" -q; then
+  primary_create
+fi
+
+STATUS=$(primary_status)
+
+if [ ${STATUS} == "Stopped" ]; then
+  primary_start
+
+  for ((n=1; n <= 10; n++)); do
+    STATUS=$(primary_status)
+
+    if [ ${STATUS} == "Running" ]; then
+      break
+    else
+      echo "Primary Multipass Machine Status (${n}): ${STATUS}"
+    fi
+
+    sleep 2
+  done
+fi
+
+config_environment_conf_file
+
+primary_stop
