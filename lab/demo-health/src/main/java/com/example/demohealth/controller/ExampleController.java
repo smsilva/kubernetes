7e6fb62ce547224100c80314af98de77e4d339a3
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-07-20 17:49:39 -0300

    Retornando informações do server

---
+++ lab/demo-health/src/main/java/com/example/demohealth/controller/ExampleController.java
@@ -0,0 +1,40 @@
+package com.example.demohealth.controller;
+
+import com.example.demohealth.config.ApplicationInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+@Controller
+public class ExampleController {
+
+    @Autowired
+    private ApplicationInfo info;
+
+    @GetMapping("/")
+    @ResponseBody
+    public ResponseEntity<ApplicationInfo>  sayHello() throws Exception {
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        final Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            final NetworkInterface networkInterface = networkInterfaces.nextElement();
+            final InetAddress inetAddress = networkInterface.getInetAddresses().nextElement();
+            if ("eth0".equalsIgnoreCase(networkInterface.getDisplayName())) {
+                responseHeaders.set("ipv4-address-" + networkInterface.getDisplayName(), inetAddress.getHostAddress());
+            }
+        }
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(info);
+    }
+
+}
