76ee42a3f251f0ddd1762e3cd3451447b3ca581a
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-07-20 14:15:50 -0300

    Passando variáveis de ambiente como parâmetro para o Health Check

--- lab/demo-health/scripts/health-check/run.sh
+++ lab/demo-health/scripts/health-check/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-INTERVAL=$1
-PORT=$2
-ENDPOINT=$3
+echo "PORT........: ${PORT}"
+echo "ENDPOINT....: ${ENDPOINT}"
+echo "INTERVAL....: ${INTERVAL}"
+
 while true; do
   NOW=$(date "+%Y-%m-%d %H:%M:%S")
-  curl -s http://localhost:${PORT:-8080}/${ENDPOINT:-"/actuator/health"} > /dev/null
-  echo "${NOW} health-check-result: $?"
+  curl -s http://localhost:${PORT:-8080}/${ENDPOINT:-"actuator/health"} > /dev/null
+  echo "${NOW} result: $?"
   sleep ${INTERVAL:-3}
 done
