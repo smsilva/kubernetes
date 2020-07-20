274a422c88461eb831f1e479354d431499f4585c
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-07-20 13:27:26 -0300

    Mais parâmetros no Health Check

--- lab/demo-health/scripts/health-check/run.sh
+++ lab/demo-health/scripts/health-check/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 INTERVAL=$1
+PORT=$2
+ENDPOINT=$3
 while true; do
   NOW=$(date "+%Y-%m-%d %H:%M:%S")
-  curl -s http://localhost:8080/actuator/health > /dev/null
+  curl -s http://localhost:${PORT:-8080}/${ENDPOINT:-"/actuator/health"} > /dev/null
   echo "${NOW} health-check-result: $?"
   sleep ${INTERVAL:-3}
 done
