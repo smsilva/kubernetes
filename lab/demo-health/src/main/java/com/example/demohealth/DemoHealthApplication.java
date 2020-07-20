d919047ce8964f10cb875d50803bdb8ce3b6f401
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-07-20 11:33:42 -0300

    Exemplo usando um side-car container para coletar métricas

---
+++ lab/demo-health/src/main/java/com/example/demohealth/DemoHealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHealthApplication.class, args);
+	}
+
+}
---
+++ lab/demo-health/src/test/java/com/example/demohealth/DemoHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
