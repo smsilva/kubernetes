55d5c19454bf620716a15fd6e927b40b32e0b4d3
Silvio Silva <smsilva@gmail.com>
2020-08-08 09:31:46 -0300

    Nova versão do Kubernetes com Minikube

--- install/minikube/run.sh
+++ install/minikube/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export MINIKUBE_IN_STYLE=false
+export MINIKUBE_IN_STYLE=false && \
 minikube start \
-  --kubernetes-version v1.17.7 \
+  --kubernetes-version v1.18.6 \
   --driver=docker \
   --network-plugin=cni
 
 kubectl config use-context minikube
 
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml  
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
