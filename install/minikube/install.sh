a99331ebdc3db2eafc936141545e5333af0104b3
Silvio Silva <smsilva@gmail.com>
2020-08-25 19:42:22 -0300

    Instalação do minikube

---
+++ install/minikube/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+
+sudo mkdir -p /usr/local/bin/
+
+sudo mv minikube /usr/local/bin/
+
