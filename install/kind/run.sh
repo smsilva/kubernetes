51c42c605a2d5cf04c28e799fe9de2a53a97f256
Silvio Silva <smsilva@gmail.com>
2020-11-11 20:55:17 -0300

    Removendo contagem de tempo personalizada

--- install/kind/run.sh
+++ install/kind/run.sh
@@ -5,10 +5,7 @@ if [ -e ${CLUSTER_NAME} ]; then
   CLUSTER_NAME="demo"
 fi
 
-SECONDS=0
-
 kind create cluster \
   --config kind-example-config.yaml \
   --name ${CLUSTER_NAME}
 
-elapsed ${SECONDS}
