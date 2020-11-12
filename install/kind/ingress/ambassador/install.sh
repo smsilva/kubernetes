5084e30353ebd1eac4aec4541ffeace5e2aafb27
Silvio Silva <smsilva@gmail.com>
2020-11-12 15:46:11 -0300

    Refatorando exemplo com Kind

--- install/kind/ingress/ambassador/install.sh
+++ install/kind/ingress/ambassador/install.sh
@@ -11,29 +11,7 @@ kubectl \
 kubectl config set-context --current --namespace ambassador
 
 kubectl \
+  -n ambassador \
   wait \
   --timeout=180s \
-  -n ambassador \
   --for=condition=deployed ambassadorinstallations/ambassador
-
-kubectl create namespace dev
-
-kubectl config set-context --current --namespace dev
-
-kubectl -n dev apply -f httpbin/
-
-kubectl \
-  -n dev \
-  wait \
-  deploy httpbin \
-  --for condition=Available \
-  --timeout=180s
-
-kubectl \
-  -n dev \
-  apply \
-  -f ingress.yaml
-
-  sleep 5
-
-  curl localhost/get
