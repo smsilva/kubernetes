610ddf7547281ff33adbce778ccbf3df3d61e235
Silvio Silva <smsilva@gmail.com>
2021-02-13 10:43:43 -0300

    Atualizando exemplo com Kind e PodDisruptionBudget

--- install/kind/monitor.sh
+++ install/kind/monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-watch -n3 'kubectl get nodes | awk "{ print \$1,\$2,\$3 }" | column -t && echo "" && \
-kubectl get deploy,rs && echo "" && \
+watch -n3 'kubectl get nodes -L disktype | awk "{ print \$1,\$2,\$3,\$6 }" | column -t && echo "" && \
+kubectl get deploy && echo "" && \
 kubectl get pods -o wide | awk "{ print \$1,\$2,\$3,\$4,\$5,\$6,\$7 }" | column -t && echo "" && \
-kubectl get ep nginx'
+kubectl get pdb'
