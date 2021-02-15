fbe192017fb5b2783d802f8b5186d7376106ba4d
Silvio Silva <smsilva@gmail.com>
2021-02-15 15:22:24 -0300

    Exemplo com Certificado no Ingress

--- install/kind/ingress/nginx/install.sh
+++ install/kind/ingress/nginx/install.sh
@@ -8,3 +8,14 @@ kubectl wait \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=180s
+
+openssl req -x509 -newkey rsa:4096 -nodes \
+  -keyout cert.key.pem \
+  -out cert.pem \
+  -days 365 \
+  -subj '/CN=app.example.com'
+
+kubectl create secret tls \
+  secret-tls-app.example.com \
+  --key cert.key.pem \
+  --cert cert.pem
