00684bdbe19d79ee947652ce670b6acee56189df
Silvio Silva <smsilva@gmail.com>
2021-02-17 11:05:36 -0300

    Atualizando exemplo com Kind

--- install/kind/2-monitor.sh
+++ install/kind/2-monitor.sh
@@ -2,5 +2,5 @@
 watch -n3 'kubectl get nodes -L disktype | awk "{ print \$1,\$2,\$3,\$6 }" | column -t && echo "" && \
 kubectl get deploy && echo "" && \
 kubectl get pods -o wide | awk "{ print \$1,\$2,\$3,\$4,\$5,\$6,\$7 }" | column -t && echo "" && \
-echo "Endpoints available: $(kubectl get ep nginx -o yaml | grep ' ip:' | wc -l)" && echo "" && \
+echo "Endpoints available: $(kubectl get ep httpbin -o yaml | grep ' ip:' | wc -l)" && echo "" && \
 kubectl get pdb'
