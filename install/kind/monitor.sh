02d4e7f7ef5bc6d1154fb6affafdb8863ecdc852
Silvio Silva <smsilva@gmail.com>
2021-02-20 10:23:18 -0300

    Atualizando exemplo com Kind/Ingress

--- install/kind/monitor.sh
+++ install/kind/monitor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-watch -n3 'kubectl get nodes -L disktype | awk "{ print \$1,\$2,\$3,\$6 }" | column -t && echo "" && \
-kubectl get deploy && echo "" && \
+watch -n 3 'kubectl get deploy && echo "" && \
 kubectl get pods -o wide | awk "{ print \$1,\$2,\$3,\$4,\$5,\$6,\$7 }" | column -t && echo "" && \
 echo "Endpoints available: $(kubectl get ep httpbin -o yaml | grep ' ip:' | wc -l)" && echo "" && \
-kubectl get pdb'
+kubectl get ingress'
