d153cfceb50f5ef63e6f7e762614e296982eb4b2
Silvio Silva <smsilva@gmail.com>
2021-04-15 10:25:56 -0300

    Vault script atualizado

--- install/aks/run.sh
+++ install/aks/run.sh
@@ -20,13 +20,13 @@ fi
 # az ad user get-member-groups --id ${USER_ID} -o table
 
 ENVIRONMENT="dev" && \
-AZ_AKS_REGION="eastus2" && \
-AZ_AKS_RESOURCE_GROUP_NAME="aks-${ENVIRONMENT}" && \
+AZ_AKS_REGION="centralus" && \
+AZ_AKS_RESOURCE_GROUP_NAME="${USER?}-${ENVIRONMENT?}-${AZ_AKS_REGION?}" && \
 AZ_AKS_ADMIN_GROUP_NAME="myAKSAdminGroup" && \
-AZ_AKS_CLUSTER_NAME="${USER}-${ENVIRONMENT}-${AZ_AKS_REGION}" && \
-AZ_AKS_CLUSTER_VERSION="$(az aks get-versions --location "${AZ_AKS_REGION}" --output table | awk '{ print $1}' | grep -v preview | sed 1,2d | head -1)" && \
-AZ_AKS_ADMIN_GROUP_ID="$(az ad group show -g ${AZ_AKS_ADMIN_GROUP_NAME} --query objectId -o tsv)" && \
-echo "USER_ID....................: ${USER_ID?}" && \
+AZ_AKS_CLUSTER_NAME="${USER?}-${ENVIRONMENT?}-${AZ_AKS_REGION?}" && \
+AZ_AKS_CLUSTER_VERSION="$(az aks get-versions --location "${AZ_AKS_REGION?}" --output table | awk '{ print $1}' | grep -v preview | sed 1,2d | head -1)" && \
+AZ_AKS_ADMIN_GROUP_ID="$(az ad group show -g ${AZ_AKS_ADMIN_GROUP_NAME?} --query objectId -o tsv)" && \
+echo "USER_ID....................: ${USER_ID}" && \
 echo "USER_EMAIL.................: ${USER_EMAIL}" && \
 echo "AZ_AKS_REGION..............: ${AZ_AKS_REGION}" && \
 echo "AZ_AKS_RESOURCE_GROUP_NAME.: ${AZ_AKS_RESOURCE_GROUP_NAME}" && \
@@ -40,18 +40,20 @@ az group create \
   --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}"
 
 az aks create \
-  --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}" \
   --name "${AZ_AKS_CLUSTER_NAME?}" \
+  --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}" \
   --kubernetes-version "${AZ_AKS_CLUSTER_VERSION?}" \
   --enable-aad \
   --network-plugin azure \
   --aad-admin-group-object-ids "${AZ_AKS_ADMIN_GROUP_ID?}" \
-  --node-count 3
+  --enable-cluster-autoscaler \
+  --min-count 1 \
+  --max-count 3
 
 az aks get-credentials \
-  --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}" \
-  --name "${AZ_AKS_CLUSTER_NAME?}"
+  --name "${AZ_AKS_CLUSTER_NAME?}" \
+  --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}"
 
 # az aks delete \
-#   --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}" \
-#   --name "${AZ_AKS_CLUSTER_NAME?}"
+  # --name "${AZ_AKS_CLUSTER_NAME?}" \
+  # --resource-group "${AZ_AKS_RESOURCE_GROUP_NAME?}"
--- lab/vault/2.vault-cluster-config.sh
+++ lab/vault/2.vault-cluster-config.sh
@@ -3,27 +3,25 @@ VAULT_KEY_FILE="vault-cluster-keys.json"
 kubectl exec vault-0 -- vault operator init \
   -key-shares=1 \
   -key-threshold=1 \
-  -format=json | tee ${VAULT_KEY_FILE}
+  -format=json | tee ${VAULT_KEY_FILE?}
 
-VAULT_UNSEAL_KEY=$(cat ${VAULT_KEY_FILE} | jq -r ".unseal_keys_b64[]")
+VAULT_UNSEAL_KEY=$(cat ${VAULT_KEY_FILE?} | jq -r ".unseal_keys_b64[]")
 
-kubectl exec vault-0 -- vault operator unseal ${VAULT_UNSEAL_KEY} && \
+kubectl exec vault-0 -- vault operator unseal ${VAULT_UNSEAL_KEY?} && \
 kubectl exec vault-0 -- vault status
 
 kubectl exec vault-1 -- vault operator raft join http://vault-0.vault-internal:8200 && \
-kubectl exec vault-1 -- vault operator unseal ${VAULT_UNSEAL_KEY}
+kubectl exec vault-1 -- vault operator unseal ${VAULT_UNSEAL_KEY?}
 kubectl exec vault-1 -- vault status
 
 kubectl exec vault-2 -- vault operator raft join http://vault-0.vault-internal:8200 && \
-kubectl exec vault-2 -- vault operator unseal ${VAULT_UNSEAL_KEY}
+kubectl exec vault-2 -- vault operator unseal ${VAULT_UNSEAL_KEY?}
 kubectl exec vault-2 -- vault status
 
-cat ${VAULT_KEY_FILE} | jq -r ".root_token" | clip && \
+cat ${VAULT_KEY_FILE?} | jq -r ".root_token" | clip && \
 kubectl exec \
   --namespace vault \
   --stdin=true \
   --tty=true vault-0 -- /bin/sh
-
-vault login
 
 vault operator raft list-peers
