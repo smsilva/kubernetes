02cdda86c9b459444847a025489020adb5160f66
Silvio Silva <smsilva@gmail.com>
2021-04-22 16:34:13 -0300

    Argo Rollouts

--- lab/argorollouts/02-istio.sh
+++ lab/argorollouts/02-istio.sh
@@ -1,13 +1,13 @@
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/istio/rollout.yaml
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/istio/services.yaml
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/istio/virtualsvc.yaml
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/istio/gateway.yaml
+BASE_URL="https://raw.githubusercontent.com/argoproj/argo-rollouts/master/docs/getting-started/istio/"
+
+kubectl apply -f ${BASE_URL}/rollout.yaml
+kubectl apply -f ${BASE_URL}/services.yaml
+kubectl apply -f ${BASE_URL}/virtualsvc.yaml
+kubectl apply -f ${BASE_URL}/gateway.yaml
 
 kubectl argo rollouts get rollout rollouts-demo
 
 kubectl argo rollouts set image rollouts-demo rollouts-demo=argoproj/rollouts-demo:yellow
-
-kubectl argo rollouts get rollout rollouts-demo
 
 kubectl argo rollouts get rollout rollouts-demo --watch
 
