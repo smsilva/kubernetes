85378ac1ee245eead98a36d8949220ce4cd873ab
Silvio Silva <smsilva@gmail.com>
2021-04-28 15:10:16 -0300

    ArgoCD

--- lab/argo/argocd/00.kind-cluster-creation.sh
+++ lab/argo/argocd/00.kind-cluster-creation.sh
@@ -5,9 +5,32 @@ apiVersion: kind.x-k8s.io/v1alpha4
 kind: Cluster
 nodes:
 - role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
 - role: worker
 EOF
 
 kind create cluster \
   --config kind-cluster.yaml \
   --name argocd
+
+kubectl \
+  apply \
+  --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+
+kubectl wait \
+  --namespace ingress-nginx \
+  --for condition=ready pod \
+  --selector app.kubernetes.io/component=controller \
+  --timeout=360s
