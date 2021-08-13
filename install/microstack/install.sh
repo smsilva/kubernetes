5707d480665aa21052192f73a573fc224a6dcb5c
Silvio Silva <smsilva@gmail.com>
2021-08-13 18:51:26 -0300

    Microstack

--- install/microstack/install.sh
+++ install/microstack/install.sh
@@ -4,10 +4,22 @@ sudo snap remove microstack --purge
 
 sudo snap install microstack --edge --devmode
 
-sudo microstack init --auto --control
+sudo microstack init \
+  --auto \
+  --control \
+  --debug \
+  --default-source-ip 192.168.68.107
 
 OS_PASSWORD=$(sudo snap get microstack config.credentials.keystone-password)
 
 echo "OS_PASSWORD: ${OS_PASSWORD}"
 
 microstack launch cirros -n test
+
+microstack.openstack image create \
+  --public \
+  --disk-format qcow2 \
+  --min-disk 20 \
+  --min-ram 2048 \
+  --file ubuntu-focal-server-cloud-amd64-20210810.img \
+  ubuntu-focal-server-cloud-amd64-20210810
