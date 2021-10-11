95368cdf337cfb1bfa707ee3d397e4cd0b3dbc2d
Silvio Silva <smsilva@gmail.com>
2021-10-11 08:04:14 -0300

    Testing Terraform Version Restriction

--- lab/crossplane/terraform-provider/show-objects.sh
+++ lab/crossplane/terraform-provider/show-objects.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-echo "Provider:                    " && echo "" && (kubectl get Provider                    ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
+#echo "Provider:                    " && echo "" && (kubectl get Provider                    ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
+echo "ProviderConfig:              " && echo "" && (kubectl get ProviderConfig              ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
 echo "CompositeResourceDefinition: " && echo "" && (kubectl get CompositeResourceDefinition ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
 echo "Compositions:                " && echo "" && (kubectl get Composition                 ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
 echo "Buckets:                     " && echo "" && (kubectl get Bucket                      ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
 echo "CompositeBucket:             " && echo "" && (kubectl get CompositeBucket             ) 2>&1 | awk '{ print "  " $0 }' && echo "" && \
 echo "PODs:                        " && echo "" && (kubectl get POD                         ) 2>&1 | awk '{ print "  " $0 }' && echo ""
 
-POD_NAME="azure-dummy-stack"
+kubectl describe workspace
 
-pods_logs() {
-  kubectl logs ${POD_NAME}
-}
-
-if kubectl get pod ${POD_NAME} &> /dev/null; then
-echo "POD Logs [${POD_NAME}]:      " && echo "" && (pods_logs                               ) 2>&1 | awk '{ print "  " $0 }' && echo ""
-fi
