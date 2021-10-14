57620ec9b4d4a2db61bf4e8a8c11854cde384d39
Silvio Silva <smsilva@gmail.com>
2021-10-14 20:50:17 -0300

    Refactor

--- lab/crossplane/terraform-provider/delete-all-crossplane-objects.sh
+++ lab/crossplane/terraform-provider/delete-all-crossplane-objects.sh
@@ -3,4 +3,5 @@ kubectl delete Bucket                      --all
 kubectl delete Composition                 --all
 kubectl delete CompositeResourceDefinition --all
 kubectl delete ProviderConfig              --all
-kubectl delete ProviderConfig              --all
+kubectl delete Configuration               --all
+kubectl delete Provider                    --all
