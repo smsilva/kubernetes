ed1c9cf938c42c6c2908c0cf1109db25f3632ae5
Silvio Silva <smsilva@gmail.com>
2021-12-08 12:01:03 -0300

    Updating cert-manager example

--- lab/cert-manager/install.sh
+++ lab/cert-manager/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# VERSION="0.15.1"
-VERSION="1.3.1"
+VERSION="1.6.1"
 
 kubectl apply \
-  --filename https://github.com/jetstack/cert-manager/releases/download/v${VERSION?}/cert-manager.yaml
+  --filename "https://github.com/jetstack/cert-manager/releases/download/v${VERSION?}/cert-manager.yaml"
 
 for DEPLOYMENT_NAME in $(kubectl --namespace cert-manager get deploy -o jsonpath='{.items[*].metadata.name}'); do
   kubectl --namespace cert-manager \
