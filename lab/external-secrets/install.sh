b8a1d9950b905830deccaa390e0e23c94d01c60a
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-02-26 17:13:35 -0300

    External Secrets ClusterSecretStore

--- lab/external-secrets/install.sh
+++ lab/external-secrets/install.sh
@@ -1,48 +1,50 @@
 #!/bin/bash
 
-# https://external-secrets.io/guides-getting-started/
+helm upgrade \
+  --install external-secrets external-secrets \
+  --repo https://charts.external-secrets.io \
+  --version 0.4.4 \
+  --namespace external-secrets \
+  --create-namespace \
+  --wait
 
-helm repo add external-secrets https://charts.external-secrets.io
-
-helm install external-secrets \
-   external-secrets/external-secrets \
-    --create-namespace \
-    --namespace external-secrets
-
-# AAD Pod Identity
-
-# Secret Store
-AZURE_KEYVAULT_URL="https://silvioswaspsandbox1.vault.azure.net"
-
-cat <<EOF | kubectl apply -f -
-apiVersion: external-secrets.io/v1alpha1
-kind: SecretStore
-metadata:
-  name: example-secret-store
-spec:
-  provider:
-    azurekv:
-      tenantId: "${ARM_TENANT_ID}"
-      vaultUrl: "${AZURE_KEYVAULT_URL}"
-      authSecretRef:
-        clientId:
-          name: azure-secret-sp
-          key: ClientID
-        clientSecret:
-          name: azure-secret-sp
-          key: ClientSecret
-EOF
-
-ARM_CLIENT_ID_BASE64=$(echo ${ARM_CLIENT_ID} | base64)
-ARM_CLIENT_SECRET_BASE64=$(echo ${ARM_CLIENT_SECRET} | base64)
+# Service Principal
+ARM_CLIENT_ID_BASE64=$(     echo ${ARM_CLIENT_ID}     | base64 )
+ARM_CLIENT_SECRET_BASE64=$( echo ${ARM_CLIENT_SECRET} | base64 )
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
-  name: azure-secret-sp
+  name: azurerm-service-principal
 type: Opaque
 data:
-  ClientID: ${ARM_CLIENT_ID_BASE64}
-  ClientSecret: ${ARM_CLIENT_SECRET_BASE64}
+  ARM_CLIENT_ID: ${ARM_CLIENT_ID_BASE64}
+  ARM_CLIENT_SECRET: ${ARM_CLIENT_SECRET_BASE64}
+EOF
+
+# Secret Store
+cat <<EOF | kubectl apply -f -
+apiVersion: external-secrets.io/v1alpha1
+kind: ClusterSecretStore
+metadata:
+  name: wasp-foundation
+spec:
+  provider:
+    azurekv:
+      authType: ServicePrincipal
+
+      tenantId: ${ARM_TENANT_ID}
+      vaultUrl: https://${ARM_KEYVAULT_NAME}.vault.azure.net
+
+      authSecretRef:
+        clientId:
+          key: ARM_CLIENT_ID
+          name: azurerm-service-principal
+          namespace: default
+
+        clientSecret:
+          key: ARM_CLIENT_SECRET
+          name: azurerm-service-principal
+          namespace: default
 EOF
