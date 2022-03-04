ba0179042e64e568fde7722f03ebc93a7898a1b8
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-04 16:19:50 -0300

    Update cluster_id variables

--- lab/argo/argocd/kustomize/aks-cluster/wasp-sbx-na-ceus-aks-a.auto.tfvars
+++ lab/argo/argocd/kustomize/aks-cluster/wasp-sbx-na-ceus-aks-a.auto.tfvars
@@ -1,3 +1,4 @@
 cluster_location = "centralus"
+cluster_id       = "ceus-a"
 cluster_name     = "wasp-sbx-na-ceus-aks-a"
 keyvault_name    = "wasp-sbx-na-ceus"
--- lab/argo/argocd/kustomize/run.sh
+++ lab/argo/argocd/kustomize/run.sh
@@ -7,7 +7,15 @@ aks-cluster/create.sh "wasp-sbx-na-ceus-aks-a"
 # ArgoCD Cluster
 kind/create-cluster.sh
 
-# External Secrets Install / Secret azurerm-service-principal / ClusterSecretStores: ["wasp-sbx-na-eus2","wasp-sbx-na-ceus"] 
+# External Secrets Install
+#
+#   - secret: azurerm-service-principal
+#
+#   - ClusterSecretStores: [
+#       "wasp-sbx-na-eus2",
+#       "wasp-sbx-na-ceus"
+#     ] 
+#
 external-secrets/install.sh
 
 # ArgoCD Install using Kustomize
