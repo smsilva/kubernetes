dc7ce718276e87b649510a4044ae7a565c9ba4c9
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-05 02:48:47 -0300

    Update argocd example

--- lab/argo/argocd/install-nginx-ingress-controller.sh
+++ lab/argo/argocd/install-nginx-ingress-controller.sh
@@ -10,3 +10,42 @@ kubectl wait \
   --for condition=ready pod \
   --selector app.kubernetes.io/component=controller \
   --timeout=360s
+
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: nginx
+  annotations:
+    ingress.kubernetes.io/rewrite-target: "/"
+spec:
+  ingressClassName: nginx
+  rules:
+    - host: app.example.com
+      http:
+        paths:
+          - pathType: Prefix
+            path: /test
+            backend:
+              service:
+                name: nginx
+                port:
+                  number: 80
+          - pathType: Prefix
+            path: /
+            backend:
+              service:
+                name: httpbin
+                port:
+                  number: 80
+EOF
+
+# kubectl get ingress nginx
+
+# kubectl describe ingress nginx
+
+# curl -ik --header "Host: app.example.com" https://127.0.0.1/get
+
+# curl -ik https://app.example.com/get
+
+# curl -ik https://app.example.com/test
