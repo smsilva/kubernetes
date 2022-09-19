11630be034f36eab6217e4df05021ee0ab259113
Silvio Silva <smsilva@gmail.com>
2022-09-19 16:06:19 -0300

    feat: Update ArgoCD example

--- lab/argo/argocd/argocd-install.sh
+++ lab/argo/argocd/argocd-install.sh
@@ -7,48 +7,35 @@ helm repo \
 helm repo \
   update argo &> /dev/null
 
-helm search repo argo/argo-cd
-
 echo ""
-echo "Helm install/upgrade..."
-echo ""
+echo "Installing argo-cd..."
 
 helm upgrade \
   --install \
   --namespace argocd \
   --create-namespace \
   argocd argo/argo-cd \
-  --values values/additional-projects.yaml \
   --values values/configs-known-hosts.yaml \
   --values values/extra-objects.yaml \
   --values values/extra-volumes.yaml \
   --values values/metrics.yaml \
   --values values/resource-customizations.yaml \
   --values values/service.yaml \
-  --wait
-
-echo ""
+  --wait &> /dev/null
 
 for DEPLOYMENT in $(kubectl \
   --namespace argocd \
   get deploy \
   --output name); do
-  echo "Waiting for: ${DEPLOYMENT}"
-
   kubectl \
     --namespace argocd \
     wait \
     --for condition=Available \
     --timeout=360s \
-    "${DEPLOYMENT}";
-
-  echo ""
+    "${DEPLOYMENT}" &> /dev/null
 done
 
 if ! which argocd &> /dev/null; then
-  echo "Need to download and install argocd CLI..."
-  
+  echo "Need to download and install argocd CLI..."  
   sh cli/install.sh
-  
-  echo ""
 fi
--- lab/argo/argocd/kind-cluster-creation.sh
+++ lab/argo/argocd/kind-cluster-creation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+kind get clusters | grep --quiet argocd || \
 kind create cluster \
   --image kindest/node:v1.24.0 \
   --config kind-cluster.yaml \
-  --name argocd
+  --name argocd > /dev/null
