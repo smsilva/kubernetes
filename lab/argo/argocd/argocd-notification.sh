3db0543697f337b8fc08530a438a5d14cc47c18b
Silvio Silva <smsilva@gmail.com>
2022-10-26 14:41:41 -0300

    feat(lab/argo/argocd/): Update with Google Chat example

--- lab/argo/argocd/argocd-notification.sh
+++ lab/argo/argocd/argocd-notification.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! [ -z "${TELEGRAM_BOT_TOKEN}" ]; then
+if [ -n "${TELEGRAM_BOT_TOKEN}" ]; then
 cat <<EOF | kubectl -n argocd apply -f -
 apiVersion: v1
 kind: Secret
@@ -7,6 +7,16 @@ metadata:
   name: argocd-notifications-secret
 stringData:
   telegram-token: ${TELEGRAM_BOT_TOKEN}
-  space-webhook-url: ${GOOGLE_CHAT_WEBHOOK_URL}
+EOF
+fi
+
+if [ -n "${GOOGLE_CHAT_WEBHOOK_URL_DEVOPS_TEAMS}" ]; then
+cat <<EOF | kubectl -n argocd apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: argocd-notifications-secret
+stringData:
+  google-chat-webhook-url-devops-team: ${GOOGLE_CHAT_WEBHOOK_URL_DEVOPS_TEAMS}
 EOF
 fi
