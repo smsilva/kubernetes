08d69271e107d2ec7431d25757882f0887101a04
Silvio Silva <smsilva@gmail.com>
2022-10-27 14:09:57 -0300

    feat(lab/argo/argocd/): Update with Google Chat example

--- lab/argo/argocd/argocd-notification.sh
+++ lab/argo/argocd/argocd-notification.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-if [ -n "${TELEGRAM_BOT_TOKEN}" ]; then
-cat <<EOF | kubectl -n argocd apply -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: argocd-notifications-secret
+kubectl_patch() {
+  KEYPAIR_DATA=$1
+  TEMPORARY_PATCH_FILE=$(mktemp)
+
+cat <<EOF > "${TEMPORARY_PATCH_FILE?}"
 stringData:
-  telegram-token: ${TELEGRAM_BOT_TOKEN}
+  ${KEYPAIR_DATA}
 EOF
+
+  kubectl \
+    --namespace argocd \
+    patch secret argocd-notifications-secret \
+    --patch-file="${TEMPORARY_PATCH_FILE?}"
+}
+
+if [ -n "${TELEGRAM_BOT_TOKEN}" ]; then
+  kubectl_patch "telegram-token: ${TELEGRAM_BOT_TOKEN}"
 fi
 
-if [ -n "${GOOGLE_CHAT_WEBHOOK_URL_DEVOPS_TEAMS}" ]; then
-cat <<EOF | kubectl -n argocd apply -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: argocd-notifications-secret
-stringData:
-  google-chat-webhook-url-devops-team: ${GOOGLE_CHAT_WEBHOOK_URL_DEVOPS_TEAMS}
-EOF
-fi
+printenv \
+| grep "^GOOGLE_CHAT_WEBHOOK_URL_" \
+| while read -r LINE; do
+  KEY=$(awk -F "=" '{ print $1 }' <<< "${LINE}" | tr '[:upper:]' '[:lower:]')
+  VALUE=$(sed 's/^[^=]*=//' <<< "${LINE}")
+
+  kubectl_patch "${KEY//_/-}: ${VALUE}"
+done
