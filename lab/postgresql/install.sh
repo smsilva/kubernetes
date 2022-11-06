eeb3b266cbbffddd5f208cf540ad4fe532aa3b45
Silvio Silva <smsilva@gmail.com>
2022-11-06 09:29:37 -0300

    feat(lab/postgresql/): Update

--- lab/postgresql/install.sh
+++ lab/postgresql/install.sh
@@ -4,10 +4,10 @@ git clone https://github.com/zalando/postgres-operator.git
 cd postgres-operator
 
 # Apply the manifests in the following order
-kubectl create -f manifests/configmap.yaml  # configuration
-kubectl create -f manifests/operator-service-account-rbac.yaml  # identity and permissions
-kubectl create -f manifests/postgres-operator.yaml  # deployment
-kubectl create -f manifests/api-service.yaml  # operator API to be used by UI
+kubectl create -f manifests/configmap.yaml # configuration
+kubectl create -f manifests/operator-service-account-rbac.yaml # identity and permissions
+kubectl create -f manifests/postgres-operator.yaml # deployment
+kubectl create -f manifests/api-service.yaml # operator API to be used by UI
 
 sleep 5
 
